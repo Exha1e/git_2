9aaf1d3eaea5d88d0ae296a00f216721109e041d
lsr <lsr@yeah.net>
2022-06-02 14:33:31 +0800

    第2次修改

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("23112");
         System.out.println("213213");
+        System.out.println("213213");
     }
 }
